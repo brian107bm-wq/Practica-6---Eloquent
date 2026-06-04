796af581149605d96ba371e5d84540a7386a796e
brian107bm-wq <brian107.bm@gmail.com>
2026-06-04 04:48:04 -0600

    Add files via upload

---
+++ app/Http/Resources/ProductoResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductoResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion,
+            'precio' => $this->precio,
+            'stock' => $this->stock,
+            'imagen' => $this->imagen,
+            'categoria_id' => $this->categoria_id,
+            'categoria_nombre' => $this->categoria?->nombre,
+        ];
+    }
+}
